eb18c5cb230b22afe48e71bfeb1de3d69077af28
nico <nicolasalurralde39@gmail.com>
2025-08-05 11:06:39 -0300

    V16

---
+++ enlint.config.js
@@ -0,0 +1,8 @@
+// eslint.config.js
+t/** @type {import('eslint').ESLint.ConfigData} */
+module.exports = {
+  root: true,
+  extends: [],    // no hereda ninguna configuración
+  rules: {},      // sin reglas activas
+  ignorePatterns: ['**/*']  // ignora todos los archivos
+};
